2ca6b0ee082413b9926e21276ce453b2d07eee3a
SherifShokry123 <107892568+SherifShokry123@users.noreply.github.com>
2024-02-13 14:46:43 +0200

    [feature] [CM-5946] [notification-center] (#15)
    * Add support for getStream Notifications methods
    
    * Implement pagination logic with the addition of LastNotificationID
    
    * Fix logging issue on production
    
    * Added error logging for failures in notifications and following feeds listeners.
    
    * Pass Camlist Logging method to GetStream SDK
    
    * Resolve override issue with the notification feed variable

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["GetStreamActivityFeed"])
     ],
     dependencies: [
-        .package(url: "https://github.com/camelan/stream-swift", exact: "1.0.16"),
+        .package(url: "https://github.com/camelan/stream-swift", branch: "feature/CM-5946/notification-center"),
         .package(url: "https://github.com/kean/Nuke", .upToNextMajor(from: "12.1.0")),
         .package(url: "https://github.com/AliSoftware/Reusable.git", .upToNextMajor(from: "4.1.2")),
         .package(url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.6.0")),
