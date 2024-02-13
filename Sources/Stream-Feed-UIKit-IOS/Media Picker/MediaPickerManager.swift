0042a770eb701b55b7e7d4f5b2ca541e9e09436d
SherifShokry123 <107892568+SherifShokry123@users.noreply.github.com>
2024-02-13 14:43:19 +0200

    [bugfix] [CM-6308] [timeline-video-thumbnail-orientation-issue] (#18)
    * Enable preferred track transform for correct orientation in video thumbnail generation
    
    * Adjusting the contentMode of the main image in the timeline post based on the media type and orientation of the associated activity.

--- Sources/Stream-Feed-UIKit-IOS/Media Picker/MediaPickerManager.swift
+++ Sources/Stream-Feed-UIKit-IOS/Media Picker/MediaPickerManager.swift
@@ -229,9 +229,11 @@ extension MediaPicker: UIImagePickerControllerDelegate, UINavigationControllerDe
 extension MediaPicker {
     private func getThumbnailImage(forUrl url: URL) -> UIImage? {
         let asset: AVAsset = AVAsset(url: url)
-        let imageGenerator = AVAssetImageGenerator(asset: asset)
         let pickedVideoDurationInSeconds = getVideoDuration(forUrl: url)
         let middleOfVideoInSeconds = pickedVideoDurationInSeconds / 2
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        // Enable preferred track transform to ensure correct orientation of the generated thumbnail image
+        imageGenerator.appliesPreferredTrackTransform = true
         
         do {
             let thumbnailTime = CMTimeMake(value: Int64(middleOfVideoInSeconds), timescale: 1)
