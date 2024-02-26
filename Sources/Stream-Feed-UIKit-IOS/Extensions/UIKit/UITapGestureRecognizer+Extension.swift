df856c26cdeff474db38d3cc185131148e1ba368
mohammedbadr2 <90341510+mohammedbadr2@users.noreply.github.com>
2024-02-27 01:10:40 +0200

    [feature] [CM-6248] [tapping-profile-images-in-comments-doesnt-direct-to-user-profiles] (#19)
    * Added move to user profile on tapping on his avatar from comments
    
    * Added tap on username from comments move to his profile behavior
    
    * Enabled user Interaction with commentLabel
    
    * Added accessibility identifier avatar-comment-button to avatar button in post details

---
+++ Sources/Stream-Feed-UIKit-IOS/Extensions/UIKit/UITapGestureRecognizer+Extension.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohammedbadr on 2/7/24.
+//
+
+import UIKit
+
+extension UITapGestureRecognizer {
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        guard let attributedText = label.attributedText else { return false }
+        
+        let mutableStr = NSMutableAttributedString.init(attributedString: attributedText)
+        mutableStr.addAttributes([NSAttributedString.Key.font : label.font!], range: NSRange.init(location: 0, length: attributedText.length))
+        
+        // If the label have text alignment. Delete this code if label have a default (left) aligment. Possible to add the attribute in previous adding.
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = label.textAlignment
+        mutableStr.addAttributes([NSAttributedString.Key.paragraphStyle : paragraphStyle], range: NSRange(location: 0, length: attributedText.length))
+        
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: mutableStr)
+
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
+                                          y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y);
+        
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x,
+                                                     y: locationOfTouchInLabel.y - textContainerOffset.y);
+        
+        var indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        if indexOfCharacter < targetRange.location {
+            indexOfCharacter += (targetRange.location - indexOfCharacter)
+        }
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+}
