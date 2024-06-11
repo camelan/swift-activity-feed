d721bf4b560483d983e154a8ea8bc8dd77a8caab
Sherif <sherif.shoukry@icloud.com>
2024-06-11 16:17:58 +0300

    [Package-Update] [Update-StreamSwift-SDK-Tag]

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["GetStreamActivityFeed"])
     ],
     dependencies: [
-        .package(url: "https://github.com/camelan/stream-swift", branch: "release-2-8-34-10-6-2024"),
+        .package(url: "https://github.com/camelan/stream-swift", .exactItem("1.0.18")),
         .package(url: "https://github.com/kean/Nuke", .exactItem("12.6.0")),
         .package(url: "https://github.com/AliSoftware/Reusable.git", .exactItem("4.1.2")),
         .package(url: "https://github.com/SnapKit/SnapKit", .exactItem("5.7.1")),
